8bd70929c0654caaa7850f8b26c44a84e92c43f7
Nick Pfeiffer <npfeiffe@stud.hs-heilbronn.de>
2021-04-25 14:45:59 +0200

    finish PUN tutorial

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Photon.Pun;
 using Photon.Pun.Demo.PunBasics;
+using Photon.Realtime;
 using UnityEngine;
 
 /// <summary>
@@ -12,24 +13,24 @@ public class PlayerManager : MonoBehaviourPunCallbacks, IPunObservable
 {
     #region IPunObservable implementation
 
-
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if (stream.IsWriting)
         {
             // We own this player: send the others our data
             stream.SendNext(IsFiring);
+            stream.SendNext(Health);
         }
         else
         {
             // Network player, receive date
             this.IsFiring = (bool) stream.ReceiveNext();
+            this.Health = (float) stream.ReceiveNext();
         }
     }
 
-
     #endregion
-    
+
     #region Private Fields
 
     [Tooltip("The Beams GameObject to control")] [SerializeField]
@@ -39,16 +40,44 @@ public class PlayerManager : MonoBehaviourPunCallbacks, IPunObservable
     bool IsFiring;
 
     #endregion
-    
-    
+
+
     #region Public Fields
-    
+
     [Tooltip("The current Health of our player")]
     public float Health = 1f;
+
+    [Tooltip("The local player instance. Use this to know if the local player is represented in the Scene")]
+    public static GameObject LocalPlayerInstance;
+
+    [Tooltip("The Player's UI GameObject Prefab")]
+    [SerializeField] public GameObject PlayerUiPrefab;
     
     #endregion
 
     #region MonoBehaviour CallBacks
+
+#if !UNITY_5_4_OR_NEWER
+/// <summary>See CalledOnLevelWasLoaded. Outdated in Unity 5.4.</summary>
+void OnLevelWasLoaded(int level)
+{
+    this.CalledOnLevelWasLoaded(level);
+}
+#endif
+
+
+    void CalledOnLevelWasLoaded(int level)
+    {
+        // check if we are outside the Arena and if it's the case, spawn around the center of the arena in a safe zone
+        if (!Physics.Raycast(transform.position, -Vector3.up, 5f))
+        {
+            transform.position = new Vector3(0f, 5f, 0f);
+        }
+
+        GameObject _uiGo = Instantiate(this.PlayerUiPrefab);
+        _uiGo.SendMessage("SetTarget", this, SendMessageOptions.RequireReceiver);
+    }
+
 
     /// <summary>
     /// MonoBehaviour method called on GameObject by Unity during early initialization phase.
@@ -63,6 +92,17 @@ public class PlayerManager : MonoBehaviourPunCallbacks, IPunObservable
         {
             beams.SetActive(false);
         }
+
+        // #Important
+        // used in GameManager.cs: we keep track of the localPlayer instance to prevent instantiation when levels are synchronized
+        if (photonView.IsMine)
+        {
+            PlayerManager.LocalPlayerInstance = this.gameObject;
+        }
+
+        // #Critical
+        // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
+        DontDestroyOnLoad(this.gameObject);
     }
 
 
@@ -81,8 +121,24 @@ public class PlayerManager : MonoBehaviourPunCallbacks, IPunObservable
         {
             Debug.LogError("<Color=Red><a>Missing</a></Color> CameraWork Component on playerPrefab.", this);
         }
+
+        if (PlayerUiPrefab != null)
+        {
+            GameObject _uiGo = Instantiate(PlayerUiPrefab);
+            _uiGo.SendMessage("SetTarget", this, SendMessageOptions.RequireReceiver);
+        }
+        else
+        {
+            Debug.LogWarning("<Color=Red><a>Missing</a></Color> PlayerUiPrefab reference on player Prefab.", this);
+        }
+        
+
+        #if UNITY_5_4_OR_NEWER
+        // Unity 5.4 has a new scene management. register a method to call CalledOnLevelWasLoaded.
+        UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoaded;
+        #endif
     }
-    
+
     /// <summary>
     /// MonoBehaviour method called on GameObject by Unity on every frame.
     /// </summary>
@@ -92,14 +148,14 @@ public class PlayerManager : MonoBehaviourPunCallbacks, IPunObservable
         {
             ProcessInputs();
         }
-        
+
 
         // trigger Beams active state
         if (beams != null && IsFiring != beams.activeInHierarchy)
         {
             beams.SetActive(IsFiring);
         }
-        
+
         if (photonView.IsMine)
         {
             this.ProcessInputs();
@@ -109,7 +165,7 @@ public class PlayerManager : MonoBehaviourPunCallbacks, IPunObservable
             }
         }
     }
-    
+
     /// <summary>
     /// MonoBehaviour method called when the Collider 'other' enters the trigger.
     /// Affect Health of the Player if the collider is a beam
@@ -122,14 +178,17 @@ public class PlayerManager : MonoBehaviourPunCallbacks, IPunObservable
         {
             return;
         }
+
         // We are only interested in Beamers
         // we should be using tags but for the sake of distribution, let's simply check by name.
         if (!other.name.Contains("Beam"))
         {
             return;
         }
+
         Health -= 0.1f;
     }
+
     /// <summary>
     /// MonoBehaviour method called once per frame for every Collider 'other' that is touching the trigger.
     /// We're going to affect health while the beams are touching the player
@@ -138,21 +197,39 @@ public class PlayerManager : MonoBehaviourPunCallbacks, IPunObservable
     void OnTriggerStay(Collider other)
     {
         // we dont' do anything if we are not the local player.
-        if (! photonView.IsMine)
+        if (!photonView.IsMine)
         {
             return;
         }
+
         // We are only interested in Beamers
         // we should be using tags but for the sake of distribution, let's simply check by name.
         if (!other.name.Contains("Beam"))
         {
             return;
         }
+
         // we slowly affect health when beam is constantly hitting us, so player has to move to prevent death.
-        Health -= 0.1f*Time.deltaTime;
+        Health -= 0.1f * Time.deltaTime;
     }
 
     #endregion
+
+#if UNITY_5_4_OR_NEWER
+    void OnSceneLoaded(UnityEngine.SceneManagement.Scene scene, UnityEngine.SceneManagement.LoadSceneMode loadingMode)
+    {
+        this.CalledOnLevelWasLoaded(scene.buildIndex);
+    }
+    
+#if UNITY_5_4_OR_NEWER
+    public override void OnDisable()
+    {
+        // Always call the base to remove callbacks
+        base.OnDisable ();
+        UnityEngine.SceneManagement.SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+#endif
+#endif
 
     #region Custom
 
